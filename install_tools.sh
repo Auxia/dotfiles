e024a5060578829b86f8ee2183795ab923ba89af
Auxia <sbhathal12@gmail.com>
2023-08-18 23:25:53 +0700

    Shell script to install everything in macOS

--- install_tools.sh
+++ install_tools.sh
@@ -1,49 +1,90 @@
 #!/bin/bash
 
-# Script to install my current workflow tools onto my new manjaro machine
+# Script to install my configs and tools on a fresh machine depending on OS
 
-# Update system
-sudo pacman -Syu
+# Check if OS is macOS
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "Installing on macOS"
 
-# Install yay
-sudo pacman -S yay
+    # Check if brew is installed
+    if ! command -v brew &> /dev/null; then
+        echo "Installing brew"
+        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    fi
 
-# Install neovim
-sudo pacman -S neovim git curl wget fzf tmux
+    # Check if git is installed
+    if ! command -v git &> /dev/null; then
+        echo "Installing git"
+        brew install git
+    fi
 
-# Install OMZ
-echo "Installing Oh My Zsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    # Check if neovim is installed
+    if ! command -v nvim &> /dev/null; then
+        echo "Installing neovim"
+        brew install neovim
+    fi
 
-# Install zsh-autosuggestions
-echo "Installing zsh-autosuggestions"
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    # Check if tmux is installed
+    if ! command -v tmux &> /dev/null; then
+        echo "Installing tmux"
+        brew install tmux
+    fi
 
-# Install zsh-syntax-highlighting
-echo "Installing zsh-syntax-highlighting"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    # Check if fzf is installed
+    if ! command -v fzf &> /dev/null; then
+        echo "Installing fzf"
+        brew install fzf
+    fi
 
-# Install zsh-completions
-echo "Installing zsh-completions"
-git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+    # Check if shell is zsh
+    if [[ "$SHELL" != "/bin/zsh" ]]; then
+        echo "Changing shell to zsh"
+        chsh -s /bin/zsh
+    fi
+    
+    # Check if Oh My Zsh is installed
+    if [ ! -d "$HOME/.oh-my-zsh" ]; then
+        echo "Installing Oh My Zsh"
+        sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    fi
 
-# Install packer.nvim
-echo "Installing packer.nvim"
-git clone --depth 1 https://github.com/wbthomason/packer.nvim\
- ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+    # Check if zsh-autosuggestions is installed
+    if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions" ]; then
+        echo "Installing zsh-autosuggestions"
+        git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    fi
+    
+    # Check if zsh-syntax-highlighting is installed
+    if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting" ]; then
+        echo "Installing zsh-syntax-highlighting"
+        git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+    fi
 
-nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+    # Check if zsh-completions is installed
+    if [ ! -d "$HOME/.oh-my-zsh/custom/plugins/zsh-completions" ]; then
+        echo "Installing zsh-completions"
+        git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+    fi
+    
+    # Check if packer.nvim is installed
+    if [ ! -d "$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim" ]; then
+        echo "Installing packer.nvim"
+        git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+        ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+    fi
 
-# Install nodejs
-echo "Installing nodejs"
-sudo pacman -S nodejs npm
+    # Check if conda is installed
+    if [ ! -d "$HOME/.miniconda3" ]; then
+        echo "Installing miniconda3"
+        wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh
+        bash Miniconda3-latest-MacOSX-arm64.sh
+        rm Miniconda3-latest-MacOSX-arm64.sh
+    fi
 
-# Install yarn
-echo "Installing yarn"
-sudo pacman -S yarn
+    # Move all the dotfiles to their
+    echo "Moving dotfiles"
+    cp -r .config/ $HOME/.config/
+    cp .zshrc $HOME/.zshrc
 
-# Install python
-echo "Installing python"
-sudo pacman -S python python-pip
-
-echo "Setup complete"
+    echo "Setup complete"
+fi
